c9cd1557b2a85eadb9297f436fe3ccc8b309ce4c
Wok <woctezuma@users.noreply.github.com>
2018-05-15 09:42:06 +0200

    PyPI setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='steamspy-api',
+    packages=['steamspy-api'],
+    version='0.1',
+    description='SteamSpy API',
+    author='Wok',
+    author_email='wok@tuta.io',
+    url='https://github.com/woctezuma/steamspy-api-pypi',
+    download_url='https://github.com/woctezuma/steamspy-api-pypi/archive/0.1.tar.gz',
+    keywords=['steam', 'steamspy', 'api'],
+    classifiers=[],
+)
