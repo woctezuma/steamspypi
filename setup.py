5394055f2edae0ad3fcd0dc5b87145011ddddc68
Wok <woctezuma@users.noreply.github.com>
2018-05-15 19:59:59 +0200

    Increase version number to v0.8

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ setup(
     install_requires=[
         'requests',
     ],
-    version='0.7',
+    version='0.8',
     description='SteamSpy API on PyPI',
     long_description='SteamSpyPI: an API for SteamSpy, written in Python 3.',
     long_description_content_type='text/x-rst',
     author='Wok',
     author_email='wok@tuta.io',
     url='https://github.com/woctezuma/steamspypi',
-    download_url='https://github.com/woctezuma/steamspypi/archive/0.7.tar.gz',
+    download_url='https://github.com/woctezuma/steamspypi/archive/0.8.tar.gz',
     keywords=['steam', 'steamspy', 'api'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
