4edf4db6c0708afaca21868c9b25fc342d07f3f0
Wok <woctezuma@users.noreply.github.com>
2018-05-15 12:35:38 +0200

    Prepare setup for release v0.2, with more package metadata.

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ import setuptools
 setup(
     name='steamspypi',
     packages=['steamspypi'],
-    version='0.1',
+    version='0.2',
     description='SteamSpy API on PyPI',
     author='Wok',
     author_email='wok@tuta.io',
     url='https://github.com/woctezuma/steamspypi',
-    download_url='https://github.com/woctezuma/steamspypi/archive/0.1.tar.gz',
+    download_url='https://github.com/woctezuma/steamspypi/archive/0.2.tar.gz',
     keywords=['steam', 'steamspy', 'api'],
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Games/Entertainment',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
 )
