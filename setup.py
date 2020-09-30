8c6c94e82a0662bae41da42da9420d402b51e24d
Wok <woctezuma@users.noreply.github.com>
2020-09-30 10:53:04 +0200

    Display README on PyPI

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
-from distutils.core import setup
-
-# noinspection PyUnresolvedReferences
 import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='steamspypi',
-    packages=['steamspypi'],
+    version='1.0.1',
+    author='Wok',
+    author_email='wok@tuta.io',
+    description='SteamSpy API on PyPI',
+    keywords=['steam', 'steamspy', 'api'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/woctezuma/steamspypi',
+    download_url='https://github.com/woctezuma/steamspypi/archive/1.0.1.tar.gz',
+    packages=setuptools.find_packages(),
     install_requires=[
         'requests',
     ],
-    version='1.0.1',
-    description='SteamSpy API on PyPI',
-    long_description='SteamSpyPI: an API for SteamSpy, written in Python 3.',
-    long_description_content_type='text/x-rst',
-    author='Wok',
-    author_email='wok@tuta.io',
-    url='https://github.com/woctezuma/steamspypi',
-    download_url='https://github.com/woctezuma/steamspypi/archive/1.0.1.tar.gz',
-    keywords=['steam', 'steamspy', 'api'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Topic :: Games/Entertainment',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3',
 )
